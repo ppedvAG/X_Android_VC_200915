02d85d6c6720c3493db54c4f77a006ca5c81c60e
KlaasHagge <klaash@ppedv.de>
2020-09-15 15:37:26 +0200

    Tag 1: Xamarin.Android (kommentiert)

--- X_Android/MainActivity.cs
+++ X_Android/MainActivity.cs
@@ -10,35 +10,63 @@ using Android.Widget;
 
 namespace X_Android
 {
+    //Jede Android-Activity steht für eine Aktion oder ein Layout, welche durch die App durchgeführt
+    //oder angezeigt wird. Der 'Code Behind' einer Activity ist eine C#-Klasse, welche mit dem 'Activity'
+    //-Attribut gekennzeichnet ist. Hier kann auch der evtl. angezeigte Titel und der verwendete Style
+    //definiert werden.
+    //Soll die Activity die zuerst angezeigte Activity (=Startseite) der App sein, muss hier die Property
+    //'MainLauncher' auf true stehen.
     [Activity(Label = "@string/app_name", Theme = "@style/AppTheme.NoActionBar", MainLauncher = true)]
     public class MainActivity : AppCompatActivity
     {
+        //Properties, in welchen die Steuerelemente des Layouts für den Zugriff durch C# abgelegt werden
         public EditText EdT_Input { get; set; }
         public Button Btn_Ok { get; set; }
 
+        //Methode, welche beim Starten (Initialisieren) der Activity ausgeführt wird
         protected override void OnCreate(Bundle savedInstanceState)
         {
+            //Aufruf der Base-OnCreate()-Methode (Grundlegende Activity-Initialisierung)
             base.OnCreate(savedInstanceState);
+            //Initialisierung der Xamarin-Essentials
             Xamarin.Essentials.Platform.Init(this, savedInstanceState);
+            //Zuweisung und Aktivierung eines Layouts (aus dem layout-Ordner) zu dieser Activity. Dies
+            //erfolgt mittels der Ressourcen-Klassen).
             SetContentView(Resource.Layout.content_main);
 
+            //Zuweisung der UI-Elemente zu den Properties mittels der FindViewById<>()-Methode,
+            //welche die Resource-Klassen nach der angegebenen Id durchsucht.
             EdT_Input = FindViewById<EditText>(Resource.Id.content_main_EdT_Input);
             Btn_Ok = FindViewById<Button>(Resource.Id.content_main_Btn_Ok);
 
+            //Zuweisung einer Methode zu einem Click-Event eines Buttons.
+            //Diese Methode kreiert einen Toast (kl. Anzeige am unteren Bildschirmrand) und zeigt ihn an.
             Btn_Ok.Click += (s, e) => Toast.MakeText(this, $"Ihre gewählte Zahl ist {EdT_Input.Text}.", ToastLength.Long).Show();
 
+            //Impliziter Intent (Verweis auf eine Activity, welche mit dem ihrem Typen zugeordneten
+            //Standartprogramm geöffnet wird) am Beispiel eines Webpage-Aufrufs im Standartbrowser.
+
+            //Zuweisung des Buttons
             Button Btn_Google = FindViewById<Button>(Resource.Id.content_main_Btn_Google);
-
+            //Erstellung des Intents
             Intent implizieterIntent = new Intent(Intent.ActionView, Android.Net.Uri.Parse("http://www.google.de"));
-
+            //Zuweisung des Click-Events mit der StartActivity()-Methode, welcher der Intent übergeben wird
             Btn_Google.Click += (s, e) => StartActivity(implizieterIntent);
 
 
+            //Expliziter Intent (Verweis auf eine Activity, welche in einer genau definierten App ausgeführt wird)
+            //Am Beispiel des Öffnens eines neuen Layouts
+
+            //Zuweisung des Buttons
             Button Btn_RandomPic = FindViewById<Button>(Resource.Id.content_main_Btn_RandomPic);
-
+            //Erstellung des Intents
             Intent explizieterIntent = new Intent(this, typeof(ShowRandomPicActivity));
-
+            //Zuweisung des Click-Events
             Btn_RandomPic.Click += (s, e) => StartActivity(explizieterIntent);
+
+            //Todo-Übung
+            Button btnTodo = FindViewById<Button>(Resource.Id.content_main_Btn_Todo);
+            btnTodo.Click += (s, e) => StartActivity(new Intent(this, typeof(TodoActivity)));
         }
 
         public override bool OnCreateOptionsMenu(IMenu menu)
